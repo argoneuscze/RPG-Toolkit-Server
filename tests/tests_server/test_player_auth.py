015120a898672e975ac5923f0d2e0058633ced3f
argoneus <argoneuscze@gmail.com>
2018-01-27 16:37:05 +0100

    reorganized server tests

---
+++ tests/tests_server/test_player_auth.py
@@ -0,0 +1,59 @@
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_player_auth(player_client, valid_character):
+    room = valid_character.room
+    password = valid_character.password
+    expected_characters = []
+
+    for char in room.characters:
+        expected_characters.append({
+            'short_name': char.short_name,
+            'full_name': char.full_name
+        })
+
+    data_in = [{
+        'command': 'identplayer',
+        'password': password,
+        'ooc_name': 'test_name'
+    }]
+
+    data_out = [
+        {
+            'command': 'auth_ok'
+        },
+        {
+            'command': 'roominfo',
+            'room_short_name': room.short_name,
+            'room_long_name': room.long_name,
+            'room_description': room.description,
+            'characters': expected_characters
+        }
+    ]
+
+    client = player_client(data_in)
+    await client.handle()
+
+    assert client.socket.has_equal_output(data_out)
+
+
+@pytest.mark.asyncio
+async def test_player_auth_invalidpass(player_client, valid_character):
+    password = 'invalid_pass'
+
+    data_in = [{
+        'command': 'identplayer',
+        'password': password,
+        'ooc_name': 'test_name'
+    }]
+
+    data_out = [{
+        'command': 'auth_failure',
+        'message': 'Invalid password.'
+    }]
+
+    client = player_client(data_in)
+    await client.handle()
+
+    assert client.socket.has_equal_output(data_out)
